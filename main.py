4a3bc940ed0f0f8630765f1d90b87bd19925fed5
Георгий Бурнаев <burnaev_01@mail.ru>
2021-11-24 11:42:44 +0400

    final

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 from Read_file import Read_file
 from ValigatorCollection import Validator_list
 from Write_file import Write_file
+import argparse
+
+parser = argparse.ArgumentParser("Input & output path")
+parser.add_argument("-read", type=str, default="1.txt", help="Read path")
+parser.add_argument("-write", type=str, default="rez.txt", help="Write path")
 
 
+pars = parser.parse_args()
 
-abs_file_read = "C:/Users/Георгий/Downloads/1.txt" #input("Input path to read file ")
-
-abs_file_write = 'C:/Users/Георгий/PycharmProjects/Lub_2/rez.txt'#input("Input path to write file ")
-
-lines = Read_file(abs_file_read)
+lines = Read_file(pars.read)
 
 data = Validator_list(lines.array_list())
 
 rez = data.count_invalid_arguments()
 
-write = Write_file(abs_file_write)
+write = Write_file(pars.write)
 write.write_file(data.array())
 
 print("Валидных записей:", rez[0])
 print("Невалидных записей:", rez[1])
-print("Ошибка в написании номера телефона: ", rez[2])
+print("Ошибка в написании электронной почты: ", rez[2])
 print("Ошибка в написании веса: ", rez[3])
 print("Ошибка в написании СНИЛСа: ", rez[4])
-print("Ошибка в написании номера паспорта: ", rez[5])
-print("Ошибка в написании профессии: ", rez[6])
+print("Ошибка в написании серии паспорта: ", rez[5])
+print("Ошибка в написании университета: ", rez[6])
 print("Ошибка в написании возраста: ", rez[7])
-print("Ошибка в написании академического разряда: ", rez[8])
+print("Ошибка в написании академической степени: ", rez[8])
 print("Ошибка в написании вероисповедания: ", rez[9])
 print("Ошибка в написании адреса: ", rez[10])
